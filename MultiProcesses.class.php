6e4869ceb1055f6f7a1e4fb23c86643e8533e0a7
yezuozuo <1062276629@qq.com>
2016-08-25 15:19:18 +0800

    init

--- MultiProcesses.class.php
+++ MultiProcesses.class.php
@@ -1,19 +1,24 @@
 <?php
 
-class MultiProcesses
-{
+/**
+ * Class MultiProcesses
+ */
+class MultiProcesses {
     /**
      * @var int 子进程数量
      */
     private $workerNum = 1;
+
     /**
      * @var bool 子进程挂掉后，自动重启
      */
     private $reActive = false;
+
     /**
      * @var int 主进程ID
      */
     private $masterPid = 0;
+
     /**
      * @var int 当前进程编号
      */
@@ -23,6 +28,7 @@ class MultiProcesses
      * @var int 当前进程ID
      */
     private $currentPid = 0;
+
     /**
      * @var array 子进程号集合
      */
@@ -43,38 +49,47 @@ class MultiProcesses
      */
     public $function = [
         'childProcessStart' => null, //子进程启动
-        'childProcessStop' => null, //子进程停止
+        'childProcessStop'  => null, //子进程停止
     ];
 
-    public function __construct(array $config = [])
-    {
+    /**
+     * MultiProcesses constructor.
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = []) {
         $this->workerNum = isset($config["workerNum"]) ? intval($config["workerNum"]) : $this->workerNum;
-        $this->reActive = isset($config["reActive"]) ? $config["reActive"] : $this->reActive;
-        $this->user = isset($config["user"]) ? $config["user"] : posix_getlogin();
-        $this->title = isset($config["title"]) ? $config["title"] : $this->title;
+        $this->reActive  = isset($config["reActive"]) ? $config["reActive"] : $this->reActive;
+        $this->user      = isset($config["user"]) ? $config["user"] : posix_getlogin();
+        $this->title     = isset($config["title"]) ? $config["title"] : $this->title;
 
         set_time_limit(0);
         ob_implicit_flush();
     }
 
-    public function start()
-    {
+    /**
+     * 开始进程
+     */
+    public function start() {
         for ($i = 0; $i < $this->workerNum; $i++) {
             $this->forkWorker($i);
         }
         $this->monitorWorker();
     }
 
-    private function forkWorker($workerId)
-    {
+    /**
+     * 启动进程
+     * @param $workerId
+     */
+    private function forkWorker($workerId) {
         $pid = pcntl_fork();
 
         $this->setProcessTitle($this->title);
         $this->setProcessUser($this->user);
-        $this->workers[$pid] = ['pid' => $pid, 'workerId' => $workerId];
+        $this->workers[$pid]   = ['pid' => $pid, 'workerId' => $workerId];
         $this->currentWorkerId = $workerId;
 
-        //父进程执行过程中，得到的fork返回值为子进程 号，而子进程得到的是0。
+        //父进程执行过程中，得到的fork返回值为子进程号，而子进程得到的是0。
         if ($pid > 0) {
             $this->currentPid = $this->masterPid = posix_getpid();
         } elseif (0 === $pid) {
@@ -88,15 +103,17 @@ class MultiProcesses
         }
     }
 
-    private function monitorWorker()
-    {
+    /**
+     * 监控进程
+     */
+    private function monitorWorker() {
         while (true) {
             $status = 0;
-            $pid = pcntl_wait($status, WUNTRACED);
+            $pid    = pcntl_wait($status, WUNTRACED);
             echo "进程ID:{$pid}, 发来信号,状态:{$status}." . PHP_EOL;
             // 子进程退出信号
             if ($pid > 0) {
-                $pid = $this->workers[$pid]['pid'];
+                $pid      = $this->workers[$pid]['pid'];
                 $workerId = $this->workers[$pid]['workerId'];
 
                 //从进程集合中删除相关信息
@@ -120,14 +137,18 @@ class MultiProcesses
                     exit("主进程退出\n");
                 }
             } else {
-                exit("主进程 异常 退出\n");
+                exit("主进程异常退出\n");
             }
         }
     }
 
-    private function setProcessUser($username)
-    {
-        // 用户名为空 或者 当前用户不是root用户
+    /**
+     * 设置进程的用户
+     * @param $username
+     * @return bool
+     */
+    private function setProcessUser($username) {
+        // 用户名为空或者当前用户不是root用户
         if (empty($username) || posix_getuid() !== 0) {
             return false;
         }
@@ -144,13 +165,15 @@ class MultiProcesses
         }
     }
 
-    private function setProcessTitle($title)
-    {
+    /**
+     * 设置进程的名字
+     * @param $title
+     * @return bool
+     */
+    private function setProcessTitle($title) {
         if (!empty($title)) {
             if (extension_loaded('proctitle') && function_exists('setproctitle')) {
                 return setproctitle($title);
-            } elseif (function_exists('cli_set_process_title')) {
-                return cli_set_process_title($title);
             } else {
                 return false;
             }
